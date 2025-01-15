a53be470efad2a617befc78a930e6b44bb159150
JosephTLockwood <joeojazz@gmail.com>
2025-01-15 18:24:12 -0500

    Add coast

--- src/main/java/frc/robot/subsystems/flywheel/FlywheelIOSIM.java
+++ src/main/java/frc/robot/subsystems/flywheel/FlywheelIOSIM.java
@@ -4,7 +4,7 @@ import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Kilograms;
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.Pounds;
-import static edu.wpi.first.units.Units.Volts;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
 
 import com.ctre.phoenix6.sim.TalonFXSimState;
 import edu.wpi.first.math.numbers.N1;
@@ -21,13 +21,13 @@ public class FlywheelIOSIM extends FlywheelIOCTRE {
   private final DCMotorSim motorSimModel;
   private final TalonFXSimState leaderSim;
   private final TalonFXSimState followerSim;
+  private final DCMotor motor = DCMotor.getKrakenX60Foc(2);
 
   public FlywheelIOSIM() {
     super();
 
     leaderSim = leader.getSimState();
     followerSim = follower.getSimState();
-    DCMotor motor = DCMotor.getKrakenX60Foc(2);
 
     Distance radius = Inches.of(1.5);
     double moi = Pounds.of(8.0).in(Kilograms) * Math.pow(radius.in(Meters), 2);
@@ -42,9 +42,24 @@ public class FlywheelIOSIM extends FlywheelIOCTRE {
     leaderSim.setSupplyVoltage(RobotController.getBatteryVoltage());
     followerSim.setSupplyVoltage(RobotController.getBatteryVoltage());
 
+    double motorVoltage = leaderSim.getMotorVoltage();
+    if (motorVoltage == 0) {
+      motorVoltage =
+          motorSimModel.getAngularVelocity().times(GEAR_RATIO).in(RadiansPerSecond)
+              / motor.KvRadPerSecPerVolt;
+      // add friction, this could be pulled outside the if to always apply friction
+      if (motorVoltage > 0.2) {
+        motorVoltage -= 0.2;
+      } else if (motorVoltage < -0.2) {
+        motorVoltage += 0.2;
+      } else {
+        motorVoltage = 0.0;
+      }
+    }
+
     // use the motor voltage to calculate new position and velocity
     // using WPILib's DCMotorSim class for physics simulation
-    motorSimModel.setInputVoltage(leaderSim.getMotorVoltageMeasure().in(Volts));
+    motorSimModel.setInputVoltage(motorVoltage);
     motorSimModel.update(0.020); // assume 20 ms loop time
 
     // Apply the new rotor position and velocity to the TalonFX;
